86a3a61c9825a55704a74170e2656c480ba49c6a
Eric-Militello <ericmilitello@icloud.com>
2022-04-27 18:34:49 -0400

    Added interaction time into activity feed page posts

--- spotify2_app/static/scripts/activity/activity.js
+++ spotify2_app/static/scripts/activity/activity.js
@@ -8,19 +8,23 @@ function createActivityFeed() {
 
    
     djangoUserInteractions.forEach(x => {
-        let span = '<a href="http://localhost:8000/u/'+x.username +'">'+x.username+'</a>';
+        let span = '<a href="http://localhost:8000/u/'+x.username +'">'+x.username+'</a>';  //add username to the span
 
-        if (x.disliked == 'False') {
+       
+        if (x.disliked == 'False')  //track like: append like to the span and make the span text green
+        {
             span += ` liked`
+            span += '\xa0\xa0\xa0\xa0\xa0 | \xa0\xa0\xa0\xa0\xa0' + x.interacted_at
             $(`#${CONST_STRING_DIV_ACTIVITY_CONTAINER}`).append(`
             <div id="parent-${x.id}" class="w-full">
             <span class = "w-full text-green-600 font-bold hover:underline">${span}</span>
             </div>
             `);
         } 
-        else 
+        else //track dislike: append dislike to the span and make the span text red
         {
             span += ` disliked`
+            span += '\xa0\xa0\xa0\xa0\xa0 | \xa0\xa0\xa0\xa0\xa0' + x.interacted_at
             $(`#${CONST_STRING_DIV_ACTIVITY_CONTAINER}`).append(`
             <div id="parent-${x.id}" class="w-full">
             <span class = "w-full text-red-600 font-bold hover:underline">${span}</span>
