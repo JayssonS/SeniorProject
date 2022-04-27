876f9683b2ef565bf4e64b9c193b19377aa579c0
Eric-Militello <ericmilitello@icloud.com>
2022-04-27 17:37:11 -0400

    Activity feed dislikes in red text

--- spotify2_app/static/scripts/activity/activity.js
+++ spotify2_app/static/scripts/activity/activity.js
@@ -10,15 +10,24 @@ function createActivityFeed() {
 
         if (x.disliked == 'False') {
             span += ` liked`
-        } else {
-            span += ` disliked`
-        }
-
-        $(`#${CONST_STRING_DIV_ACTIVITY_CONTAINER}`).append(`
+            $(`#${CONST_STRING_DIV_ACTIVITY_CONTAINER}`).append(`
             <div id="parent-${x.id}" class="w-full">
             <span class = "w-full text-green-600 font-bold hover:underline">${span}</span>
             </div>
-        `);
+            `);
+        } 
+        else 
+        {
+            span += ` disliked`
+            $(`#${CONST_STRING_DIV_ACTIVITY_CONTAINER}`).append(`
+            <div id="parent-${x.id}" class="w-full">
+            <span class = "w-full text-red-600 font-bold hover:underline">${span}</span>
+            </div>
+            `);
+        }
+
+        
+       
 
         createTrackElement(`parent-${x.id}`, x.track_id);
     });
