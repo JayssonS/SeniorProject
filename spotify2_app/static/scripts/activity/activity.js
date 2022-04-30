0e4c7ce8d2df4c5715881ef946e3fb16776c4546
Eric-Militello <ericmilitello@icloud.com>
2022-04-30 15:58:41 -0400

    current

--- spotify2_app/static/scripts/activity/activity.js
+++ spotify2_app/static/scripts/activity/activity.js
@@ -21,9 +21,10 @@ $(function() {
             case CONST_STRING_BTN_SELECT_FOLLOWING_ACTVITY:
                 selectedDiv = $(`#${CONST_STRING_DIV_ACTIVTY_FOLLOWING_CONTAINER}`);
                 
-                
-                buildFollowingContainer();
-                
+                if (!selectedDiv.length) {
+                    buildFollowingContainer();
+                    return;
+                }    
                 selectedDiv.removeClass('hidden');
                 break;
             
@@ -38,55 +39,6 @@ $(function() {
     buildGlobalFeed();
 });
 
-function buildFollowingFeed() {
-    djangoFollowingInteraction.forEach(x => {
-        let span = '<a href="http://localhost:8000/u/'+x.username +'">'+x.username+'</a>';  //add username to the span | links to user's profile page
-        let interaction_time_span = x.interacted_at
-        
-        if (x.disliked == 'False')  //track like interaction: append like to the span and make the span text green
-        {
-            
-            span += ` liked`   //append liked to user name
-    
-            //append user liked span and interaction time span to a div in green
-            $(`#${CONST_STRING_DIV_ACTIVTY_FOLLOWING_CONTAINER}`).append(`
-            <div id="parent-${x.id}" class="w-full border-solid border-2 border-green-600 rounded bg-slate-800">
-                <div class="flex justify-center">
-                    <span class = "w-full text-green-600 font-bold hover:underline">${span}</span>
-                    <span class = "w-full text-neutral-content text-left font-bold">${interaction_time_span}</span>
-                </div>
-            </div>
-            `);
-        } 
-        else    //track dislike interaction: append dislike to the span and make the span text red
-        {
-            span += ` disliked` //append disliked to user name
-    
-            //append user disliked span and interaction time span to a div in red
-            $(`#${CONST_STRING_DIV_ACTIVTY_FOLLOWING_CONTAINER}`).append(`
-            <div id="parent-${x.id}" class="w-full flec items-end border-solid border-2 border-red-600 rounded bg-slate-800">
-                <div class="flex justify-center">
-                    <span class = "w-full text-red-600 font-bold hover:underline">${span}</span>
-                    <span class = "w-full text-neutral-content text-left font-bold ali">${interaction_time_span}</span>
-                </div>
-            </div>
-            `);
-        }
-    
-        
-       
-        //add track widget element
-        createTrackElement(`parent-${x.id}`, x.track_id);
-    
-       
-    
-        
-       
-    });
-
-}
-
-
 function buildFollowingContainer() {
     $(`#${CONST_STRING_DIV_SEARCH_RESULTS_PLACEHOLDER}`).append(`
         <div id="${CONST_STRING_DIV_ACTIVTY_FOLLOWING_CONTAINER}"
@@ -98,10 +50,7 @@ function buildFollowingContainer() {
     buildFollowingFeed();
 }
 
-
-
 function buildGlobalFeed() {
-
     djangoUserInteractions.forEach(x => {
         let span = '<a href="http://localhost:8000/u/'+x.username +'">'+x.username+'</a>';  //add username to the span | links to user's profile page
         let interaction_time_span = x.interacted_at
@@ -139,8 +88,7 @@ function buildGlobalFeed() {
             `);
         }
 
-        
-       
+
         //add track widget element
         createTrackElement(`parent-${x.id}`, x.track_id);
        
@@ -150,4 +98,43 @@ function buildGlobalFeed() {
 }
 
 
+function buildFollowingFeed() {
+    djangoFollowingInteraction.forEach(x => {
+        let span = '<a href="http://localhost:8000/u/'+x.username +'">'+x.username+'</a>';  //add username to the span | links to user's profile page
+        let interaction_time_span = x.interacted_at
+        
+        if (x.disliked == 'False')  //track like interaction: append like to the span and make the span text green
+        {
+            
+            span += ` liked`   //append liked to user name
+    
+            //append user liked span and interaction time span to a div in green
+            $(`#${CONST_STRING_DIV_ACTIVTY_FOLLOWING_CONTAINER}`).append(`
+            <div id="parent-follow-${x.id}" class="w-full border-solid border-2 border-green-600 rounded bg-slate-800">
+                <div class="flex justify-center">
+                    <span class = "w-full text-green-600 font-bold hover:underline">${span}</span>
+                    <span class = "w-full text-neutral-content text-left font-bold">${interaction_time_span}</span>
+                </div>
+            </div>
+            `);
+        } 
+        else    //track dislike interaction: append dislike to the span and make the span text red
+        {
+            span += ` disliked` //append disliked to user name
+    
+            //append user disliked span and interaction time span to a div in red
+            $(`#${CONST_STRING_DIV_ACTIVTY_FOLLOWING_CONTAINER}`).append(`
+            <div id="parent-follow-${x.id}" class="w-full flec items-end border-solid border-2 border-red-600 rounded bg-slate-800">
+                <div class="flex justify-center">
+                    <span class = "w-full text-red-600 font-bold hover:underline">${span}</span>
+                    <span class = "w-full text-neutral-content text-left font-bold ali">${interaction_time_span}</span> 
+                </div>
+            </div>
+            `);
+        }
+        
+        //add track widget element
+        createTrackElement(`parent-follow-${x.id}`, x.track_id);  
+    });
 
+}
